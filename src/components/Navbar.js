4e5975a4b7969b7a25ef80ed7e998cd0fc64e78d
Chad <61545754+ChadH28@users.noreply.github.com>
2020-07-04 17:10:56 +0200

    Project Restart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,27 +4,30 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
-  <nav className="navbar bg-light">
-    <h1><i className={icon} /> {title}</h1>
-    <ul className="navbar-nav">
-        <li className="nav-item">
-            <Link to="/">Home</Link>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-secondary">
+    <h2 class="navbar-brand" href="#"><i className={icon} /> {title}</h2>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>   
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <Link to="/" class="nav-link">Home</Link>
         </li>
-        <li className="nav-item">
-            <Link to="/aboutme">About</Link>
+        <li class="nav-item">
+          <Link to="/aboutme" class="nav-link">About</Link>
         </li>
-
-        <li className="nav-item">
-            <Link to="/portfolio">Portfolio</Link>
+        <li class="nav-item">
+          <Link to="/portfolio" class="nav-link">Portfolio</Link>
         </li>
-        <li className="nav-item">
-            <Link to="/testimonials">Testimonials</Link>
+        <li class="nav-item">
+          <Link to="/testimonials" class="nav-link">Testimonials</Link>
         </li>
-        <li className="nav-item">
-        <Link to="/contactme">Contact details</Link>
+        <li class="nav-item">
+          <Link to="/contactme" class="nav-link">Contact details</Link>
         </li>
-    </ul>
-
+      </ul>
+    </div>
 </nav>
   );
 };
