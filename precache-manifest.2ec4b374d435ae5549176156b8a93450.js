b8388e3453b41db847b2d769b7d8e6b855cf57b4
Chad <61545754+ChadH28@users.noreply.github.com>
2021-02-14 21:53:30 +0200

    Updates

--- precache-manifest.2ec4b374d435ae5549176156b8a93450.js
+++ precache-manifest.2ec4b374d435ae5549176156b8a93450.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d05bb8fe1ed84a643d75a58b07970972",
+    "revision": "cba000d24a9270d4562e2ecd2a7c5c45",
     "url": "/Online-Portfolio/index.html"
   },
   {
-    "revision": "5954cea9a88ad65ac213",
+    "revision": "8e1a169efbedc935631a",
     "url": "/Online-Portfolio/static/css/main.a3a9fc73.chunk.css"
   },
   {
-    "revision": "6a4105bed28611021c8f",
-    "url": "/Online-Portfolio/static/js/2.573b9784.chunk.js"
+    "revision": "c643d3d0eba7cec9333b",
+    "url": "/Online-Portfolio/static/js/2.79dfd932.chunk.js"
   },
   {
     "revision": "ffa35a34964860fdc97ed79ca9abd27c",
-    "url": "/Online-Portfolio/static/js/2.573b9784.chunk.js.LICENSE.txt"
+    "url": "/Online-Portfolio/static/js/2.79dfd932.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5954cea9a88ad65ac213",
-    "url": "/Online-Portfolio/static/js/main.139e2b5c.chunk.js"
+    "revision": "8e1a169efbedc935631a",
+    "url": "/Online-Portfolio/static/js/main.3117e47a.chunk.js"
   },
   {
     "revision": "c0e5b6bee1a3e0eb49eb",
@@ -32,6 +32,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Online-Portfolio/static/media/GitFinder.9f0cf63b.png"
   },
   {
+    "revision": "584d9f322f08684db8ff2217731e474f",
+    "url": "/Online-Portfolio/static/media/bootstrap-4.584d9f32.png"
+  },
+  {
     "revision": "9e3dadac96d0ccd44c63fa7a40a8a182",
     "url": "/Online-Portfolio/static/media/chad-removebg.9e3dadac.png"
   },
@@ -44,6 +48,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Online-Portfolio/static/media/contact_assistant.09c7c534.png"
   },
   {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/Online-Portfolio/static/media/css.75e4d6d5.png"
+  },
+  {
     "revision": "d4e4cbb4c9c4ad562c36a0b210a862cf",
     "url": "/Online-Portfolio/static/media/e-commerce.d4e4cbb4.jpg"
   },
@@ -52,8 +60,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Online-Portfolio/static/media/godwin.60e56287.jpg"
   },
   {
+    "revision": "387273e516903c231d3cd0272cfd9f22",
+    "url": "/Online-Portfolio/static/media/html-5.387273e5.png"
+  },
+  {
     "revision": "639e7c9be51b99c51be12db16bbdbec0",
     "url": "/Online-Portfolio/static/media/it-logger.639e7c9b.jpg"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/Online-Portfolio/static/media/javascript.6a4c201c.png"
   },
   {
     "revision": "e04148989caddd3551d55e4fcce5fea0",
@@ -64,12 +80,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Online-Portfolio/static/media/module_project.f9733394.png"
   },
   {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/Online-Portfolio/static/media/nodejs.55e83371.png"
+  },
+  {
     "revision": "b4e390ae109891d2eb773eb3feef0e0a",
     "url": "/Online-Portfolio/static/media/oslin.b4e390ae.jpeg"
   },
   {
     "revision": "7e3bbaa1eca934118b0b691e1ee4dca0",
     "url": "/Online-Portfolio/static/media/pong.7e3bbaa1.jpg"
+  },
+  {
+    "revision": "6dd7bc34be27074a7e889b7dba23247e",
+    "url": "/Online-Portfolio/static/media/python.6dd7bc34.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/Online-Portfolio/static/media/react.f297dfcd.png"
   },
   {
     "revision": "46ac037fad4c5c81250a8a55206b4535",
@@ -82,5 +110,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1b3413f8f363f04d7a1a03ba6b7d3ceb",
     "url": "/Online-Portfolio/static/media/snake.1b3413f8.jpg"
+  },
+  {
+    "revision": "e4d536b1a07294afad43045def94294f",
+    "url": "/Online-Portfolio/static/media/sql-server.e4d536b1.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Online-Portfolio/precache-manifest.c4eab2371caa625ed27b286651c9e06e.js"
+  "/Online-Portfolio/precache-manifest.2ec4b374d435ae5549176156b8a93450.js"
 );
 
 self.addEventListener('message', (event) => {
