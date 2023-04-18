5ce8666d5afbc7f0b5009522f63d8449eaa8c404
Chad Hoosain <chad@bnry.digital>
2023-04-18 09:14:12 +0200

    put content in a datajs file

---
+++ src/components/landing/LandingPage.data.js
@@ -0,0 +1,14 @@
+export const data = {
+  content: {
+    heading: "- Chad Hoosain",
+    sub_heading: "Based in South Africa",
+    personality_span: [
+      "Assertive",
+      "Hardworking",
+      "Dedicated",
+      "Analytical",
+      "Friendly",
+      "Creative",
+    ],
+  },
+};
--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,40 +1,30 @@
 import React from "react";
 import "./LandingPage.css";
 import chad from "../assets/landing-img.png";
-import $ from "jquery";
+import { data } from "./LandingPage.data";
 
 const LandingPage = () => {
-
-  $('.hero-img').onclick = () =>
-    function () {
-      $(".landing-img").ripples("show");
-    };
-
-  var personality_span = [
-    "Hardworking",
-    "Dedicated",
-    "Analytical",
-    "Friendly",
-    "Creative"
-  ];
-
   textSequence3(0);
   function textSequence3(i) {
-    if (personality_span.length > i) {
+    if (data.content.personality_span.length > i) {
       setTimeout(function () {
-        document.getElementById("span-sequence").innerHTML = personality_span[i];
+        document.getElementById("span-sequence").innerHTML =
+          data.content.personality_span[i];
         textSequence3(++i);
       }, 5000);
-    } else if (personality_span.length === i) {
+    } else if (data.content.personality_span.length === i) {
       // Loop
       textSequence3(0);
     }
   }
 
   return (
-    <section id='landing-page' className="container">
+    <section id="landing-page" className="container">
       <div className="row">
-        <div className="col-sm-5 col-md-4 col-lg-4 hero-img" data-aos="zoom-out-down">
+        <div
+          className="col-sm-5 col-md-4 col-lg-4 hero-img"
+          data-aos="zoom-out-down"
+        >
           <img src={chad} alt="CH" className="landing-img" />
         </div>
         <div className="col-sm-7 col-md-8 col-lg-8 hero">
@@ -42,17 +32,20 @@ const LandingPage = () => {
             // id="sequence"
             className="animate__animated animate__bounce header "
           >
-            <strong>- Chad Hoosain</strong>
+            <strong>{data.content.heading}</strong>
           </h1>
           {/* <hr /> */}
           <p
-            // id="sub-sequence" 
+            // id="sub-sequence"
             className="sub-header"
             data-aos="zoom-in-right"
           >
-            <span id="span-sequence" data-aos="zoom-out-down" >Assertive</span> <span className='profession'>Web Developer</span>
+            <span id="span-sequence" data-aos="zoom-out-down">
+              Assertive
+            </span>{" "}
+            <span className="profession">Web Developer</span>
           </p>
-          <h2 data-aos="zoom-out-up">Based in South Africa</h2>
+          <h2 data-aos="zoom-out-up">{data.content.sub_heading}</h2>
         </div>
       </div>
     </section>
