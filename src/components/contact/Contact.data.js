2dc87bc48409bb134bbf98364920c25020ac743a
Chad Hoosain <chad@bnry.digital>
2023-04-20 13:16:26 +0200

    added socials to a datajs file

---
+++ src/components/contact/Contact.data.js
@@ -0,0 +1,34 @@
+export const data = {
+  socials: [
+    {
+      link: "tel:084-466-4783",
+      type: "call",
+      icon: "fas fa-phone",
+      fade_duration: "1600",
+    },
+    {
+      link: "https://mail.google.com/mail/u/0/#inbox?compose=new",
+      type: "google",
+      icon: "far fa-envelope",
+      fade_duration: "1800",
+    },
+    {
+      link: "https://www.linkedin.com/in/chad-hoosain-3660641a6/",
+      type: "linkedin",
+      icon: "fab fa-linkedin-in",
+      fade_duration: "2000",
+    },
+    {
+      link: "https://github.com/ChadH28",
+      type: "github",
+      icon: "fab fa-github",
+      fade_duration: "2200",
+    },
+    {
+      link: "https://codepen.io/chadh28",
+      type: "code-pen",
+      icon: "fab fa-codepen",
+      fade_duration: "2400",
+    },
+  ],
+};
